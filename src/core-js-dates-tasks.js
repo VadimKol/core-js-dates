25d091186f0ebaa94d409715aef8bc41880ea86e
Vadim Kolymbet <3t0n@inbox.ru>
2024-03-19 15:46:23 +0300

    feat: solve isDateInPeriod task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -153,8 +153,11 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  return (
+    new Date(period.end) - new Date(date) >= 0 &&
+    new Date(period.start) - new Date(date) <= 0
+  );
 }
 
 /**
